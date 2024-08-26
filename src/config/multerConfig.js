1cd3ae27c3f252b2a98f5521dbadd352c97103c8
ermias alehegn <ermiaseat1987@gmail.com>
2024-08-26 11:46:35 +0300

    for vercel

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
+  destination: "../public/uploads",
   filename: (req, file, cb) => {
     cb(
       null,
